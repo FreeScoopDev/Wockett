9aaca8f4d7d4467c55e74c6eec3665bc88914664
Joe <josephx10@hotmail.com>
2026-08-27 01:41:57 +0000

    Add WockettTests unit test target
    Created via Xcode's New Target wizard: PBXNativeTarget wired to
    PoCSquat as its TestTargetID, bundle id Scoops.WockettTests, TEST_HOST
    pointed at the PoCSquat.app build product.

---
+++ WockettTests/WockettTests.swift
@@ -0,0 +1,18 @@
+//
+//  WockettTests.swift
+//  WockettTests
+//
+//  Created by Joe Amanatidis on 8/26/26.
+//
+
+import Testing
+
+struct WockettTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
